2d9f0410adcbbc6e99aae2f844efa73ee2048c2e
tabat <tabatarov.12@gmail.com>
2024-02-26 00:33:45 +0100

    Ultimo

--- MauiApp2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp2.AssemblyInfo.cs
+++ MauiApp2/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/MauiApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7361048de19464e36d622cbff0fa7fb867f78902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a09f97937d3080e51e0a58fc890adc86fa96199b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
